464ea1d19a0de0f51d81ac68c661073af40331f3
Evan Carr <37350180+EvanDAB@users.noreply.github.com>
2019-01-22 12:17:26 -0800

    scrapesmetacritic

---
+++ init/big_scrape.py
@@ -0,0 +1,62 @@
+import requests
+import bs4
+from urllib import urlopen
+from bs4 import BeautifulSoup
+
+# match = soup.title        // only title with its tags around it
+# match = soup.title.text   // only title text
+# match = soup.div          // all div elements and all the elements with the div elements
+# match = soup.find('footer', class_='page-footer')  find specific element and all the elements within it
+
+def main(searchItem):
+    url = 'http://www.metacritic.com/game/playstation-4/' + searchItem + '/'
+    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+    headers={'User-Agent':user_agent,}
+
+    r = requests.get(url, headers=headers)
+    stat = r.status_code
+    text = r.text
+    soup = BeautifulSoup(text, 'lxml')
+    titleHead = soup.title
+
+    gameTitle = soup.find('div', class_='product_title')
+    gameTitleText = gameTitle.a.h1.text
+    print(gameTitleText)
+
+    metaRating = soup.find('div', class_='metascore_w')
+    metaRatingText = metaRating.span.text
+    print(metaRatingText)
+
+
+    userRating = soup.find('div', class_='user')
+    userRatingText = userRating.text
+    print(userRatingText)
+
+
+    summary = soup.find('span', class_='blurb')
+    sumText = summary.text
+    print(sumText)
+
+
+    dev = soup.find('li', class_="developer")
+    devSpan = dev.findAll('span')
+    devText = devSpan[1].text
+    print(devText)
+
+    gen = soup.find('li', class_="product_genre")
+    genSpan = gen.findAll('span')
+    for i in genSpan[1:]: 
+        print(i.text)
+
+    esrb = soup.find('li', class_="product_rating")
+    esrbSpan = esrb.findAll('span')
+    esrbText = esrbSpan[1].text
+    print(esrbText)
+
+def search():
+    inp = raw_input("What game are you looking for? ")
+    print(inp)
+    main(inp)
+
+search()
+
