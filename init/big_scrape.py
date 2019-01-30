11affe033fdb84b05e893f6ebc54fa7e6f953f5c
Evan Carr <37350180+EvanDAB@users.noreply.github.com>
2019-01-30 09:56:47 -0800

    created select game platform function

--- init/big_scrape.py
+++ init/big_scrape.py
@@ -8,74 +8,79 @@ from bs4 import BeautifulSoup
 # match = soup.div          // all div elements and all the elements with the div elements
 # match = soup.find('footer', class_='page-footer')  find specific element and all the elements within it
 
-def scrape(searchItem):
-    print('======= Loading =======')
+def scrape(platform, searchItem):
+    try: 
+        print('======= Loading =======')
 
-    url = 'http://www.metacritic.com/game/playstation-4/' + searchItem + '/'
-    url2 = 'http://www.metacritic.com/game/playstation-4/' + searchItem + '/critic-reviews'
+        url = 'http://www.metacritic.com/game/'+ platform +'/' + searchItem + '/'
+        url2 = 'http://www.metacritic.com/game/'+ platform +'/' + searchItem + '/critic-reviews'
 
-    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-    headers={'User-Agent':user_agent,}
+        user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+        headers={'User-Agent':user_agent,}
 
-    r = requests.get(url, headers=headers)
-    r3 = requests.get(url2, headers=headers)
+        r = requests.get(url, headers=headers)
+        r3 = requests.get(url2, headers=headers)
 
-    stat = r.status_code
+        stat = r.status_code
 
-    text = r.text
-    text2 = r3.text 
+        text = r.text
+        text2 = r3.text 
 
-    soup = BeautifulSoup(text, 'lxml')
-    soup2 = BeautifulSoup(text2, 'lxml')
+        soup = BeautifulSoup(text, 'lxml')
+        soup2 = BeautifulSoup(text2, 'lxml')
 
-    gameTitle = soup.find('div', class_='product_title')
-    gameTitleText = gameTitle.a.h1.text
-    metaRating = soup.find('div', class_='metascore_w')
-    metaRatingText = metaRating.span.text
-    userRating = soup.find('div', class_='user')
-    userRatingText = userRating.text
-    print('Product Title: ' + gameTitleText)
+        gameTitle = soup.find('div', class_='product_title')
+        gameTitleText = gameTitle.a.h1.text
+        metaRating = soup.find('div', class_='metascore_w')
+        metaRatingText = metaRating.span.text
+        userRating = soup.find('div', class_='user')
+        userRatingText = userRating.text
+        print('Product Title: ' + gameTitleText)
 
-    summary = soup.find('span', class_='blurb')
-    sumText = summary.text
-    print('Summary: ' + sumText)
+        summary = soup.find('span', class_='blurb')
+        sumText = summary.text
+        print('Summary: ' + sumText)
 
 
-    dev = soup.find('li', class_="developer")
-    devSpan = dev.findAll('span')
-    devText = devSpan[1].text
-    print('Developer: ' + devText)
+        dev = soup.find('li', class_="developer")
+        devSpan = dev.findAll('span')
+        devText = devSpan[1].text
+        print('Developer: ' + devText)
 
-    gen = soup.find('li', class_="product_genre")
-    genSpan = gen.findAll('span')
-    print('Genres: ')
-    for i in genSpan[1:]: 
-        print( i.text)
+        gen = soup.find('li', class_="product_genre")
+        genSpan = gen.findAll('span')
+        print('Genres: ')
+        for i in genSpan[1:]: 
+            print( i.text)
 
-    esrb = soup.find('li', class_="product_rating")
-    esrbSpan = esrb.findAll('span')
-    esrbText = esrbSpan[1].text
-    print('ESRB Rating: ' + esrbText)
-    print('Metascore rating: ' + metaRatingText)
-    print('Metascore UserRating: ' + userRatingText)
+        esrb = soup.find('li', class_="product_rating")
+        esrbSpan = esrb.findAll('span')
+        esrbText = esrbSpan[1].text
+        print('ESRB Rating: ' + esrbText)
+        print('Metascore rating: ' + metaRatingText)
+        print('Metascore UserRating: ' + userRatingText)
 
-    ign = soup2.find('a', string='IGN')
-    ignP = ign.parent.parent.parent
-    ignS = ignP.find('div', class_="review_grade")
-    ignST = ignS.div.text
-    print('IGN: ' + ignST)
+        ign = soup2.find('a', string='IGN')
+        ignP = ign.parent.parent.parent
+        ignS = ignP.find('div', class_="review_grade")
+        ignST = ignS.div.text
+        print('IGN: ' + ignST)
 
-    gi = soup2.find('a', string='Game Informer')
-    giP = gi.parent.parent.parent
-    giS = giP.find('div', class_="review_grade")
-    giST = giS.div.text
-    print('GameInformer: ' + giST)
+        gi = soup2.find('a', string='Game Informer')
+        giP = gi.parent.parent.parent
+        giS = giP.find('div', class_="review_grade")
+        giST = giS.div.text
+        print('GameInformer: ' + giST)
 
-    gs = soup2.find('a', string='GameSpot')
-    gsP = gs.parent.parent.parent
-    gsS = gsP.find('div', class_="review_grade")
-    gsST = gsS.div.text
-    print('GameSpot: ' + gsST)
+        gs = soup2.find('a', string='GameSpot')
+        gsP = gs.parent.parent.parent
+        gsS = gsP.find('div', class_="review_grade")
+        gsST = gsS.div.text
+        print('GameSpot: ' + gsST)
+
+    except AttributeError as inst:
+        print('Sorry we were unable to find that title')
+        print(inst)
 
 
 
--- init/scrape.py
+++ init/scrape.py
@@ -1,14 +1,18 @@
 from big_scrape import scrape as scrape
 
 def search():
+    #first search
     inp1 = raw_input("What is the first game you are looking for? ")
     inp1 = inp1.replace(' ','-').lower()
+    platform1 = raw_input("On which platform? ")
+    platform1 = platform1.replace(' ','-').lower()
+    scrape(platform1, inp1)
 
+    #second search
     inp2 = raw_input("What is the second game you are looking for? ")
     inp2 = inp2.replace(' ','-').lower()
-
-    scrape(inp1)
-    print('=======  Now looking for the second game =======')
-    scrape(inp2)
-
+    platform2 = raw_input("On which platform? ")
+    platform2 = platform2.replace(' ','-').lower()
+    scrape(platform2, inp2)
+    
 search()
